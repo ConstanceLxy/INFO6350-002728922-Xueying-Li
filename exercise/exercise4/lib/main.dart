4ebd4a7e28fa607472e57cc3d4cf94a52f526a80
ConstanceLxy <li.xueyin@Northeastern.edu>
2023-11-16 19:47:02 -0800

    update

---
+++ exercise/exercise4/lib/main.dart
@@ -0,0 +1,506 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class PhotoHero extends StatelessWidget {
+  const PhotoHero({
+    super.key,
+    required this.photo,
+    this.onTap,
+    required this.width,
+  });
+
+  final String photo;
+  final VoidCallback? onTap;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: Hero(
+        tag: photo,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            child: Image.asset(
+              photo,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  StaggerAnimation({super.key, required this.controller})
+      :
+
+        // Each animation defined here transforms its value during the subset
+        // of the controller's duration defined by the animation's interval.
+        // For example the opacity animation transforms its value during
+        // the first 10% of the controller's duration.
+
+        opacity = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.0,
+              0.100,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        width = Tween<double>(
+          begin: 50.0,
+          end: 150.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.125,
+              0.250,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        height = Tween<double>(begin: 50.0, end: 150.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        padding = EdgeInsetsTween(
+          begin: const EdgeInsets.only(bottom: 16),
+          end: const EdgeInsets.only(bottom: 75),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        borderRadius = BorderRadiusTween(
+          begin: BorderRadius.circular(4),
+          end: BorderRadius.circular(75),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.375,
+              0.500,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        color = ColorTween(
+          begin: Colors.indigo[100],
+          end: Colors.orange[400],
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.500,
+              0.750,
+              curve: Curves.ease,
+            ),
+          ),
+        );
+
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius?> borderRadius;
+  final Animation<Color?> color;
+
+  // This function is called each time the controller "ticks" a new frame.
+  // When it runs, all of the animation's values will have been
+  // updated to reflect the controller's current value.
+  Widget _buildAnimation(BuildContext context, Widget? child) {
+    return Container(
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+            color: color.value,
+            border: Border.all(
+              color: Colors.indigo[300]!,
+              width: 3,
+            ),
+            borderRadius: borderRadius.value,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
+    );
+  }
+}
+
+class RadialExpansion extends StatelessWidget {
+  RadialExpansion({
+    super.key,
+    required this.minRadius,
+    required this.maxRadius,
+    this.child,
+  }) : clipTween = Tween<double>(
+          begin: 2.0 * minRadius,
+          end: 2.0 * (maxRadius / math.sqrt2),
+        );
+
+  final double minRadius;
+  final double maxRadius;
+  final Tween<double> clipTween;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, size) {
+        final double t =
+            (size.biggest.width / 2.0 - minRadius) / (maxRadius - minRadius);
+        final double rectClipExtent = clipTween.transform(t);
+        return ClipOval(
+          child: Center(
+            child: SizedBox(
+              width: rectClipExtent,
+              height: rectClipExtent,
+              child: ClipRect(
+                child: child,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Photo extends StatelessWidget {
+  const Photo({super.key, required this.photo, this.onTap});
+
+  final String photo;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // Slightly opaque color appears where the image has transparency.
+      color: Theme.of(context).primaryColor.withOpacity(0.25),
+      child: InkWell(
+        onTap: onTap,
+        child: LayoutBuilder(
+          builder: (context, size) {
+            return Image.asset(
+              photo,
+              fit: BoxFit.contain,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// #docregion LogoWidget
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({super.key});
+
+  // Leave out the height and width so it fills the animating parent
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: const FlutterLogo(),
+    );
+  }
+}
+// #enddocregion LogoWidget
+
+// #docregion GrowTransition
+class GrowTransition extends StatelessWidget {
+  const GrowTransition(
+      {required this.child, required this.animation, super.key});
+
+  final Widget child;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return SizedBox(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+// #enddocregion GrowTransition
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller.forward().orCancel;
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {
+      // The animation got canceled, probably because we were disposed.
+    }
+  }
+
+  static const opacityCurve = Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
+
+  RectTween _createRectTween(Rect? begin, Rect? end) {
+    return MaterialRectCenterArcTween(begin: begin, end: end);
+  }
+
+  Widget _buildPage(
+      BuildContext context, String imageName, String description) {
+    return Container(
+      color: Theme.of(context).canvasColor,
+      child: Center(
+        child: Card(
+          elevation: 8,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Hero(
+                  createRectTween: _createRectTween,
+                  tag: imageName,
+                  child: RadialExpansion(
+                    minRadius: 100,
+                    maxRadius: 100,
+                    child: Photo(
+                      photo: imageName,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                description,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 3,
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHero(
+    BuildContext context,
+    String imageName,
+    String description,
+  ) {
+    return SizedBox(
+      width: 100,
+      height: 100,
+      child: Hero(
+        createRectTween: _createRectTween,
+        tag: imageName,
+        child: RadialExpansion(
+          minRadius: 100,
+          maxRadius: 100,
+          child: Photo(
+            photo: imageName,
+            onTap: () {
+              Navigator.of(context).push(
+                PageRouteBuilder<void>(
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return AnimatedBuilder(
+                      animation: animation,
+                      builder: (context, child) {
+                        return Opacity(
+                          opacity: opacityCurve.transform(animation.value),
+                          child: _buildPage(context, imageName, description),
+                        );
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Flippers Page'),
+                    ),
+                    body: Container(
+                      padding: const EdgeInsets.all(8),
+                      alignment: Alignment.topLeft,
+                      // Use background color to emphasize that it's a new route.
+                      color: Colors.lightBlueAccent,
+                      child: Hero(
+                        tag: 'flippers',
+                        child: SizedBox(
+                          width: 50,
+                          child: Image.asset(
+                            'images/flippers-alpha.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+          // Main route
+          child: Hero(
+            tag: 'flippers',
+            child: Image.asset(
+              width: 150,
+              'images/flippers-alpha.png',
+            ),
+          ),
+        ),
+        PhotoHero(
+          photo: 'images/flippers-alpha.png',
+          width: 100,
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute<void>(
+              builder: (context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text('Flippers Page'),
+                  ),
+                  body: Container(
+                    // Set background to blue to emphasize that it's a new route.
+                    color: Colors.lightBlueAccent,
+                    padding: const EdgeInsets.all(16),
+                    alignment: Alignment.topLeft,
+                    child: PhotoHero(
+                      photo: 'images/flippers-alpha.png',
+                      width: 100,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                );
+              },
+            ));
+          },
+        ),
+        GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            _playAnimation();
+          },
+          child: Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.1),
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              child: StaggerAnimation(controller: _controller.view),
+            ),
+          ),
+        ),
+        _buildHero(context, 'images/hair-alpha.png', 'Chair'),
+        GrowTransition(
+          animation: animation,
+          child: const LogoWidget(),
+        )
+      ]),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    ),
+  );
+}
